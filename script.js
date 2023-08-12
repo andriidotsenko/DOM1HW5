d23c806e017cff96574c5bdf2226e85e02fab1ae
andriidotsenko <andrui.dots@gmail.com>
2023-08-12 19:46:36 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// 1. Задача на селектори
+
+//  для елементу з текстом 'Навігація по DOM дереву'
+headerTwo = document.getElementById('headerTwo')
+
+console.log(headerTwo)
+
+//======================================================
+// для першого елементу <section>
+section = document.getElementsByTagName('section')[0]
+
+console.log(section)
+
+//=======================================================
+// для елементу списку з текстом 'Пункт 5'
+function queryItemFive() {
+	let listItems = document.querySelectorAll('li')
+	let resultArr = []
+	for (let i = 0; i < listItems.length; i++) {
+		if (listItems[i].textContent === 'Пункт 5') {
+			resultArr.push(listItems[i])
+			return resultArr[0]
+		}
+	}
+}
+
+console.log(queryItemFive())
+
+//========================================================
+// для елементу з класом 'hatredLevelBlock'
+
+hatredLevelBlock = document.getElementsByClassName('hatredLevelBlock')[0]
+console.log(hatredLevelBlock);
